0aee83b1fc84310664417096be535113375af762
ismael <ifsjunior2@gmail.com>
2023-05-10 16:47:27 -0300

    Feat(Created folder assets)

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="m-auto antialiased text-center">
       <header className="bg-white-900 min-h-screen flex-col items-center justify-center text-white text-2xl">
-        <img src={logo} className="h-48" alt="logo" />
+        <img src={logo} className="h-48 flex-col items-center justify-center" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
