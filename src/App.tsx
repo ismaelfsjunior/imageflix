f08255833b9722d999840c6d32512adc851a8502
ismael <ifsjunior2@gmail.com>
2023-05-10 17:38:42 -0300

    Feat(Created folder components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 //import './App.css';
 import './input.css';
+import Hero from './components/Hero';
 
-function App() {
+const App = () => {
   return (
-    <div className="m-auto antialiased text-center">
-      <header className="bg-white-900 min-h-screen flex-col items-center justify-center text-white text-2xl">
-        <img src={logo} className="h-48 flex-col items-center justify-center" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-300"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-auto antialiased bg-black text-white">
+      <Hero />
     </div>
   );
 }
