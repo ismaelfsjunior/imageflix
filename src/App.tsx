2386cf867a51ac857282309a4e71b7e69ff28c62
ismael <ifsjunior2@gmail.com>
2023-05-10 15:54:03 -0300

    feat(Configuração do Tailwind)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './input.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="m-auto antialiased text-center">
+      <header className="bg-white-900 min-h-screen flex-col items-center justify-center text-white text-2xl">
+        <img src={logo} className="h-48" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-blue-300"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
