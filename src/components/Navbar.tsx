136d4fd65cec35294967f2e9b0d511bd6216ecd9
ismael <ifsjunior2@gmail.com>
2023-05-11 18:21:58 -0300

    Feat(finalizando o navbar)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import logo from "../assets/imagiflix.svg";
 import { faSearch, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import user from "../assets/user.jpg";
 
-//21 parte 6
-const NavBar = () => (
-    <nav className="absolute top-0 left-0 grid grid-cols-2 items-center w-full p-8">
+import './Navbar.css';
+
+const NavBar = () => {
+    const [isMenuOpen, setIsMenuOpen ] = useState(false);
+
+    const handleClick = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+   return (
+    <nav className="navbar absolute top-0 left-0 grid grid-cols-2 items-center w-full p-8">
         <div className="justify-self-start grid grid-cols-2 gap-4 items-center">
             <h1 className="hidden">Imagiflix</h1>
             <img src={logo} className="w-36" alt="Imagiflix" />
@@ -19,23 +27,23 @@ const NavBar = () => (
         </div>
         <div className="justify-self-end flex justify-items-end items-center">
             <form className="relative">
-                <input className="w-full bg-black" type="text" placeholder="Título, gente e gêneros" />
-                <button className="abosolute right-2"><FontAwesomeIcon icon={faSearch} /></button>
+                <input className="w-full bg-black border border-white rounded py-1 px-3 transition-all duration-300 ease-in-out opacity-0 hover:opacity-100 focus:opacity-100" type="text" placeholder="Título, gente e gêneros" />
+                <button className="search absolute right-0 py-1 px-2" onClick={ e => e.preventDefault()}><FontAwesomeIcon icon={faSearch} /></button>
             </form>
-            <div className="flex ml-4">
-                <img src={user} alt="Foto do usuário" />
-                <button>
+            <div className="relative flex ml-4">
+                <img src={user} alt="Foto do usuário" onClick={handleClick}/>
+                <button onClick={handleClick}>
                     <FontAwesomeIcon className="ml-2" icon={faCaretDown} />
                 </button>
-                <ul className="hidden">
-                    <li><a href="#account">Minha Conta</a></li>
-                    <li><a href="#logout">Sair</a></li>
+                <ul className={`absolute pl-2 mt-8 top-2 right-0 w-32 bg-black rounded py-1 transition-all duration-500 ease-in-out ${!isMenuOpen && 'opacity-0 invisible'}`}>
+                    <li><a href="#account" onClick={handleClick}>Minha Conta</a></li>
+                    <li><a href="#logout" onClick={handleClick}>Sair</a></li>
                 </ul>
             </div>
         </div>
     
     </nav>
-
-);
+    );
+};
 
 export default NavBar;
