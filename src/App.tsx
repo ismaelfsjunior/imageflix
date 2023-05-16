0a916c58ef88e01dab6cb3893a15d8c6355af945
ismael <ifsjunior2@gmail.com>
2023-05-16 12:26:35 -0300

    feat(Created component loading)

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,23 @@ import Hero from './components/Hero';
 import NavBar from './components/Navbar';
 import Footer from './components/Footer';
 import Carrousel from './components/Carrousel';
+import Loading from './components/Loading';
 import './input.css';
 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+
+import { Title } from './data/mockData';
 
 const App = () => {
   const {URL, APISTRING } = CONST;
 
   const [movies , setMovies ] = useState<any[]>([]);
   const [series, setSeries ] = useState<any[]>([]);
+  const [title, setTitle] = useState<any>();
   const moviesUrl = `${URL}/discover/movie${APISTRING}&sort_by=popularity.desc`
   const serieUrl = `${URL}/discover/tv${APISTRING}&sort_by=popularity.desc`
+  const [loading, setLoading] = useState<boolean>(true)
 
   useEffect( () => {
     const fetchData = async () => {
@@ -25,7 +30,9 @@ const App = () => {
         setMovies(moviesData.data.results);
 
         const serieData = await axios.get(serieUrl);
-        setSeries(serieData.data.results);        
+        setSeries(serieData.data.results);   
+        
+        setLoading(false);
       } catch (error) {
         setMovies([]);
         setSeries([]);
@@ -39,19 +46,36 @@ const App = () => {
   
   const getMovieList = () => {
     if (movies) {
-      const [featured, ...movieList] = movies;     
+      const [...movieList] = movies;     
       return movieList;
     }
     return [];
   }
 
+  const getTitle = async ({ type, id }: Title) => {
+    setLoading(true);
+    const title = await axios.get(`${URL}/${type}/${id}${APISTRING}`)
+    setTitle(title.data);
+    setLoading(false);
+  };
+
   return (
     <div className="m-auto antialiased bg-black text-white">
-      <Hero {...getFeaturedMovie()}/>
-      <NavBar />
-      <Carrousel title='Filmes' data={getMovieList()}/>
-      <Carrousel title='Serie' data={series}/>
-      <Carrousel title='outros'/>
+      {loading && (
+        <>
+          <Loading />
+          <NavBar />
+        </>
+      )}
+      {!loading && (
+        <>
+        <Hero {...getFeaturedMovie()}/>
+        <NavBar />
+        <Carrousel title='Filmes' data={getMovieList()}/>
+        <Carrousel title='Serie' data={series}/>
+        <Carrousel title='outros'/>
+        </>
+      )}
       <Footer />
     </div>
   );
